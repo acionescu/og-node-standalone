02713c8d94e450c8d1cf6e833cc99ed6a726e0d3
Adrian Ionescu <adrian.ionescu@snowglobegames.com>
2020-11-28 12:04:52 +0200

    starting readme

--- java/main/test/net/segoia/ogeg/node/NodeBootstrap.java
+++ java/main/test/net/segoia/ogeg/node/NodeBootstrap.java
@@ -3,10 +3,10 @@ package net.segoia.ogeg.node;
 public class NodeBootstrap {
     
     public static void main(String[] args) throws Exception{
-//	new StandaloneOgNode("config/node-config1.json").start();
+	new StandaloneOgNode("test/node-config1.json").start();
 //	Thread.sleep(5000);
 //	new StandaloneOgNode("config/node-config2.json").start();
-	new StandaloneOgNode("test/node-config3.json").start();
+//	new StandaloneOgNode("test/node-config3.json").start();
     }
 
 }
