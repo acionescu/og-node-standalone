c1dedb2454db9fd7d081593dc0cf219c0ded3ba3
Adrian Ionescu <adrian.ionescu@snowglobegames.com>
2021-11-12 13:36:27 +0200

    configuring cef-node

--- scripts/conf.sh
+++ scripts/conf.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+export OGNODE_JAVA_HOME=/home/adi/Downloads/openjdk-11
+export CEF_BIN_DIR=/home/adi/Downloads/linux64_cef/java-cef-build-bin/bin
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source conf.sh
-java -Dlog4j.configuration=file:config/log4j.properties -Djava.net.preferIPv4Stack=true -cp libs/*:og-node.jar net.segoia.ogeg.node.OgNodeStarter config/node-init.json
+$OGNODE_JAVA_HOME/bin/java -Dlog4j.configuration=file:config/log4j.properties -Djava.net.preferIPv4Stack=true -Djava.library.path=$CEF_BIN_DIR/lib/linux64 -cp libs/*:cef/libs/*:cef-node.jar net.segoia.ogeg.node.OgNodeStarter config/node-init.json
